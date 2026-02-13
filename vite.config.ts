e0dc337fa312afd2a85768f5f38cdbefa62da0e9
Gian Andrew Del Rosario <gian@Gians-MacBook-Air.local>
2026-02-13 20:26:32 +0800

    host was added

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
 	plugins: [react(), tailwindcss()],
 
 	preview: {
-		allowedHosts: ['*.ondigitalocean.app', '.ondigitalocean.app', '206.189.86.201'],
+		allowedHosts: ['*.ondigitalocean.app', '.ondigitalocean.app', '64.225.44.255'],
 	},
 	server: {
-		allowedHosts: ['*.ondigitalocean.app', '.ondigitalocean.app', '206.189.86.201'],
+		allowedHosts: ['*.ondigitalocean.app', '.ondigitalocean.app', '64.225.44.255'],
 	},
 });
